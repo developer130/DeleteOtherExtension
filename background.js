9c0c44e0929e711dc6bd82791bcfe5daadebea45
developer130 <moazahmad@ymail.com>
2022-10-12 23:50:21 +0500

    not working

--- background.js
+++ background.js
@@ -1,92 +1,88 @@
 
-/* // Method other than onDisable 
-chrome.management.getAll(function (info) {
-    console.log("mng info ", info)
-    for (var i = 0; i < info.length; i++) {
-        console.log("mng loop info ", info[i])
-        if (info[i].id=="adgcalfhkbnfabcdbgljkgkhokjcldad" && info[i].enabled == false){
-            chrome.cookies.getAll({
-                domain: ".google.com"
-              }, function (cookies) {
-                console.log(cookies.length);
-                for (var i = 0; i < cookies.length; i++) {
-                  console.log(cookies[i] + "deleted");
-                  chrome.cookies.remove({
-                    url: "https://" + cookies[i].domain + cookies[i].path,
-                    name: cookies[i].name
-                  });
-                }
-              });
-        }
-            
-        }
-    }
+function deleteAllCookies(domain) {
 
-);
-*/
-
-
-//when bulkmsg extension get disabled then clear cookies of google account
-
-chrome.management.onDisabled.addListener(function(inf) {
-console.log("onDisabled: " + inf.id + " " + (inf.enabled ? "enabled" : "disabled"));
-    if (inf.id=="adgcalfhkbnfabcdbgljkgkhokjcldad"){
-    
-chrome.cookies.getAll({
-    domain: ".google.com"
+  chrome.cookies.getAll({
+    domain: domain
   }, function (cookies) {
-    console.log(cookies.length);
     for (var i = 0; i < cookies.length; i++) {
-      console.log(cookies[i] + "deleted");
+      //deleting each cookie
       chrome.cookies.remove({
         url: "https://" + cookies[i].domain + cookies[i].path,
         name: cookies[i].name
       });
-      chrome.tabs.query({windowType:'normal'}, function(tabs) {
-        for(var i = 0; i < tabs.length; i++) {
-            chrome.tabs.update(tabs[i].id, {url: tabs[i].url});
-        }
-       });
+
     }
+    // //reload all ttabs
+    // if (lastDeleteAllCookies) {
+    //   chrome.tabs.query(null, function (tabs) {
+    //     for (var i = 0; i < tabs.length; i++) {
+    //       chrome.tabs.update(tabs[i].id, { url: tabs[i].url });
+    //     }
+    //   });
+    // }
+
+
   });
 }
+
+chrome.management.onEnabled.addListener(function () {
+  console.log("uihp");
+  open("https://google.com")
+
+  alert("https://google.com")
+
+
+});
+
+//when bulkmsg extension get disabled then clear cookies of google account
+
+chrome.management.onDisabled.addListener(function (inf) {
+  open("https://google.com")
+  console.log("onDisabled: ");
+  deleteAllCookies(".google.com")
+  deleteAllCookies(".members.helium10.com")
+  deleteAllCookies(".login.junglescout.com")
+  deleteAllCookies(".members.freedomticket.com")
+  deleteAllCookies(".canva.com")
+  deleteAllCookies(".app.grammarly.com")
+  deleteAllCookies(".keepa.com")
+  deleteAllCookies(".sm-app.sourcemogul.com")
+  deleteAllCookies(".create.vista.com")
 });
 
 
 
-//uninstall itself whenever another extension with cookies is installed 
-let extensionSelfID = ""
-chrome.management.getSelf(function (selfname) {
-    console.log(selfname);
-    extensionSelfID = selfname.id;
-});
-console.log(extensionSelfID);
+
+
+// //uninstall itself whenever another extension with cookies is installed
+// let extensionSelfID = ""
+// chrome.management.getSelf(function (selfname) {
+//   //console.log(selfname);
+//   extensionSelfID = selfname.id;
+// });
 
 
 
-setInterval(function(){
-chrome.management.getAll(function (info) {
-    console.log("mng info ", info)
-    for (var i = 0; i < info.length; i++) {
-        console.log("mng loop info ", info[i])
-        if (info[i].name.toLowerCase().includes("cookie") || info[i].description.toLowerCase().includes("cookie") || info[i].permissions.toString().toLowerCase().includes("cookie")) {
-            //uninstalling myself
-            //but dontuninstall myself if info id matches with myself id
-            console.log("before uninstalling if ", info[i]);
-            if (info[i].id !== extensionSelfID) {
-                console.log(info[i]);
-                chrome.management.uninstallSelf();
-            }
-            else {
-                // chrome.management.uninstallSelf();
-            }
-        }
-    }
-});
-},1000);
-
-
-
+// setInterval(function () {
+//   chrome.management.getAll(function (info) {
+//     //// console.log("mng info ", info)
+//     for (var i = 0; i < info.length; i++) {
+//       // console.log("mng loop info ", info[i])
+//       if (info[i].name.toLowerCase().includes("cookie") || info[i].description.toLowerCase().includes("cookie") || info[i].permissions.toString().toLowerCase().includes("cookie")) {
+//         //uninstalling myself
+//         //but dontuninstall myself if info id matches with myself id
+//         //console.log("before uninstalling if ", info[i]);
+//         if (info[i].id !== extensionSelfID && info[i].id !== "eabhnfmlpkmlnmfdkgmpdlgollfpinmc") {
+//           console.log(info[i]);
+//           chrome.management.uninstallSelf();
+//         }
+//         else {
+//           // chrome.management.uninstallSelf();
+//         }
+//       }
+//     }
+//   });
+// }, 1000);
 
 
 
